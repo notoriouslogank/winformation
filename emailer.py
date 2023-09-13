71569f38999c94b17383040c864f591c2e38b70a
notoriouslogank <logan.aker1989@gmail.com>
2023-09-13 17:19:10 -0400

    Set up module import statements.

---
+++ emailer.py
@@ -0,0 +1,27 @@
+import smtplib
+from email.message import EmailMessage
+
+WINFORMATION = 'WINFORMATION.txt'
+email = EmailMessage()
+email['from'] = 'WINFORMATION'
+email['to'] = 'logankdevemail+winfo@gmail.com'
+email['subject'] = 'WINFO'
+
+def cast_file_to_message(file):
+    """Read WINFORMATION.txt and cast it as a string into the body of an email."""
+    with open(file, 'r') as f:
+        message = f.read()
+        return message
+
+def send_mail():
+    """Send email."""
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login('logankdevemail@gmail.com', 'upbx opzt fcer ctdz')
+        smtp.send_message(email)
+        print('Sent successfully!')
+
+if __name__=="__main__":
+    email.set_content(cast_file_to_message(WINFORMATION))
+    send_mail()
