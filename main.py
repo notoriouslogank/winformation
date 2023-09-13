f6115fb5ec573b41bf58b2a95532bce2156bac5c
notoriouslogank <logan.aker1989@gmail.com>
2023-09-13 17:15:49 -0400

    Restructed filesystem (will probably need to be fixed later); made imports work; now using main.py as main function.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import emailer
 import getsysinfo
-import sys
+
+WINFORMATION = 'WINFORMATION.txt'
+getsysinfo.get_sysinfo()
+emailer.cast_file_to_message(WINFORMATION)
+emailer.send_mail()
 #import logging
 
 #logging.basicConfig()
-def main():
-    getsysinfo()
-    emailer()
-    sys.exit()
-main()
+""" def main():
+    getsysinfo.get_sysinfo()
+    emailer.cast_file_to_message()
+    emailer.send_mail()
+
+main() """
