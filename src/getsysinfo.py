c5203f102054c69e0b3306aa83b1c745028bdc50
notoriouslogank <logan.aker1989@gmail.com>
2023-09-13 15:38:36 -0400

    Added some docstrings.

--- src/getsysinfo.py
+++ src/getsysinfo.py
@@ -14,11 +14,15 @@ def get_sysinfo():
         write_outfile(entry)
         
 def clean_entry(entry):
+    """Sanitize each line of the output before writing
+    """
     cleaned_entry = (str(entry).strip('[\']')).strip() # Remove leading and trailing whitespaces and Python-related [] and '
     cleaned_entry = " ".join(cleaned_entry.split()) # Remove consecutive whitespace characters (to make output smaller onscreen)
     return cleaned_entry # Returns one individual entry, *NOT* the entire outfile; there might be a better way to do this
 
 def write_outfile(line):
+    """Write each line of the system info to an output file
+    """
     # TODO: Send this outfile to the root dir
     # TODO: Or, maybe make it write to a tmp file here then format it later?
     with open('WINFORMATION.txt', 'a') as output:
