5f024c6891523b64d95959ce32c2ea0b452a60e7
notoriouslogank <logan.aker1989@gmail.com>
2023-09-13 15:38:09 -0400

    Added fucntionality to send WINFORMATION.txt as email text.

--- src/emailer.py
+++ src/emailer.py
@@ -1,18 +1,24 @@
-## Module to send the emails, once created
-
 import smtplib
 from email.message import EmailMessage
 
+WINFORMATION = 'WINFORMATION.txt'
 email = EmailMessage()
 email['from'] = 'WINFORMATION'
 email['to'] = 'logankdevemail+winfo@gmail.com'
 email['subject'] = 'WINFO'
 
-email.set_content('Hello')
+def cast_file_to_message(file):
+    with open(file, 'r') as f:
+        message = f.read()
+        return message
 
-with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.login('logankdevemail@gmail.com', 'upbx opzt fcer ctdz')
-    smtp.send_message(email)
-    print('Check your email.')
+def send_mail():
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login('logankdevemail@gmail.com', 'upbx opzt fcer ctdz')
+        smtp.send_message(email)
+        print('Check your email.')
+
+email.set_content(cast_file_to_message(WINFORMATION))
+send_mail()
