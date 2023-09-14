722788d25fdc898c954522adf0d34ea7db633224
logank <logan.aker1989@gmail.com>
2023-09-13 21:39:09 -0400

    Implemented logging.

--- emailer.py
+++ emailer.py
@@ -1,12 +1,18 @@
 import smtplib
 import os
+import logging
 from dotenv import load_dotenv
 from email.message import EmailMessage
 
+logging.basicConfig(filename='winfo.log', filemode='a', level=logging.DEBUG)
+
 load_dotenv()
 USER = os.getenv('USERNAME')
+logging.debug(f'USER = {USER}')
 PASS = os.getenv('PASSWORD')
-WINFORMATION = os.getenv('WINFORMATION')
+logging.debug(f'PASS successfully sourced from .env')
+WINFO = os.getenv('WINFORMATION')
+logging.debug(f'WINFO = {WINFO}')
 
 email = EmailMessage()
 email['from'] = 'WINFORMATION'
@@ -15,8 +21,10 @@ email['subject'] = 'WINFO'
 
 def cast_file_to_message(file):
    """Read WINFORMATION.txt and cast it as a string into the body of an email."""
+   logging.debug('Casting file to message...')
    with open(file, 'r') as f:
         message = f.read()
+        logging.debug('File cast to message.')
         return message
 
 def send_mail():
@@ -26,8 +34,10 @@ def send_mail():
         smtp.starttls()
         smtp.login(USER, PASS)
         smtp.send_message(email)
-        print('Sent successfully!')
+        logging.debug('Message sent!')
+        print('Message sent successfully!')
+
 
 if __name__=="__main__":
-    email.set_content(cast_file_to_message(WINFORMATION))
+    email.set_content(cast_file_to_message(WINFO))
     send_mail()
