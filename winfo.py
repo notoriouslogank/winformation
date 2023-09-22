da1b79afed87cce733cfc89d9ced2f4300a9e856
logank <logan.aker1989@gmail.com>
2023-09-21 22:14:35 -0400

    Remove unused imports.

--- winfo.py
+++ winfo.py
@@ -1,12 +1,9 @@
 import emailer as e
-import os
-from dotenv import load_dotenv
 import getsysinfo
 
 
 if __name__ == "__main__":
-    load_dotenv()
     SYS_INFO = "SYS_INFO.txt"
-    getsysinfo.isPlatformSupported() # This should probably be renamed, I guess
+    getsysinfo.isPlatformSupported()  # This should probably be renamed, I guess
     e.email.set_content(e.cast_file_to_message(SYS_INFO))
     e.send_mail()
