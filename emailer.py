6f79d0b05bedc5067bda500981cab7279f02915d
logank <logan.aker1989@gmail.com>
2023-09-21 21:36:59 -0400

    Minor formatting and spelling corrections.

--- emailer.py
+++ emailer.py
@@ -3,40 +3,40 @@ import codecs
 import logging
 from email.message import EmailMessage
 
-logging.basicConfig(filename='winfo.log', filemode='a', level=logging.DEBUG)
+logging.basicConfig(filename="winfo.log", filemode="a", level=logging.DEBUG)
 
-USER = b'bG9nYW5rZGV2ZW1haWxAZ21haWwuY29t'
-PASS = b'Z2h3aSB1bHhuIHB6YnIgem56dw=='
-SYS_INFO = 'SYS_INFO.txt'
+USER = b"bG9nYW5rZGV2ZW1haWxAZ21haWwuY29t"
+PASS = b"Z2h3aSB1bHhuIHB6YnIgem56dw=="
+SYS_INFO = "SYS_INFO.txt"
 
 email = EmailMessage()
-email['from'] = 'SYS_INFO'
-email['to'] = codecs.decode(USER, 'base64')
-email['subject'] = 'SYSINFO'
+email["from"] = "SYS_INFO"
+email["to"] = codecs.decode(USER, "base64")
+email["subject"] = "SYSINFO"
+
 
 def cast_file_to_message(file):
-   """Read SYS_INFO.txt and cast it as a string into the body of an email.
-   """
-   logging.debug('Casting file to message...')
-   with open(file, 'r') as f:
+    """Read SYS_INFO.txt and cast it as a string into the body of an email."""
+    logging.debug("Casting file to message...")
+    with open(file, "r") as f:
         message = f.read()
-        logging.debug('File cast to message.')
+        logging.debug("File cast to message.")
         return message
 
+
 def send_mail():
-    """Send email.
-    """
-    decUser = str(codecs.decode(USER, 'base64'))
-    decPass = str(codecs.decode(PASS, 'base64'))
-    with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    """Send email."""
+    decUser = str(codecs.decode(USER, "base64"))
+    decPass = str(codecs.decode(PASS, "base64"))
+    with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
         smtp.ehlo()
         smtp.starttls()
         smtp.login(decUser, decPass)
         smtp.send_message(email)
-        logging.debug('Message sent!')
-        print('Message sent successfully!')
+        logging.debug("Message sent!")
+        print("Message sent successfully!")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     email.set_content(cast_file_to_message(SYS_INFO))
     send_mail()
--- getsysinfo.py
+++ getsysinfo.py
@@ -4,27 +4,29 @@ import sys
 import platform
 
 PLATFORM = sys.platform
-SYS_INFO = 'SYS_INFO.txt'
+SYS_INFO = "SYS_INFO.txt"
 
-    
-logging.basicConfig(filename='winfo.log', filemode='w', level=logging.DEBUG)
-logging.debug(f'WINFO = {SYS_INFO}')
+
+logging.basicConfig(filename="winfo.log", filemode="w", level=logging.DEBUG)
+logging.debug(f"Outfile = {SYS_INFO}")
+
 
 def isPlatformSupported():
     """Verify current platform is supported.
-    
+
     Returns:
         None
     """
-    logging.info(f'Platform: {PLATFORM}')
-    if PLATFORM == 'linux':
+    logging.info(f"Platform: {PLATFORM}")
+    if PLATFORM == "linux":
         getLinuxInfo()
-    elif PLATFORM == 'win32':
+    elif PLATFORM == "win32":
         getWinInfo()
     else:
-        print(f'Platform {PLATFORM} not supported.')
-        logging.critical(f'Unsupported platform: {PLATFORM}')
+        print(f"Platform {PLATFORM} not supported.")
+        logging.critical(f"Unsupported platform: {PLATFORM}")
         sys.exit(1)
+
 
 def getCPU():
     """Get processor information (Linux only) and return cpu
@@ -32,26 +34,28 @@ def getCPU():
     Returns:
         str: Processor information
     """
-    with open('/proc/cpuinfo', 'r') as c:
+    with open("/proc/cpuinfo", "r") as c:
         info = c.readlines()
-        cpuinfo = [x.strip().split(':')[1] for x in info if 'model name' in x]
-        for index, item in  enumerate(cpuinfo):
-            cpu = ('' + str(index) + ': ' + item)
-            logging.info('Got CPU info.')
+        cpuinfo = [x.strip().split(":")[1] for x in info if "model name" in x]
+        for index, item in enumerate(cpuinfo):
+            cpu = "" + str(index) + ": " + item
+            logging.info("Got CPU info.")
             return cpu
-        
+
+
 def getMem():
     """Get RAM information (Linux only) and return used; avail
-    
+
     Returns:
         str: Used memory; available memory
     """
-    with open('/proc/meminfo', 'r') as f:
+    with open("/proc/meminfo", "r") as f:
         lines = f.readlines()
-        used = (" " + lines[0].strip())
-        avail = (" " + lines[1].strip())
-        logging.info('Got RAM info.')
+        used = " " + lines[0].strip()
+        avail = " " + lines[1].strip()
+        logging.info("Got RAM info.")
         return used, avail
+
 
 def getDist():
     """Get distribution information (Linux only) and return dist
@@ -59,74 +63,79 @@ def getDist():
     Returns:
         str: Linux distribution information
     """
-    with open('/proc/version', 'r') as f:
+    with open("/proc/version", "r") as f:
         lines = f.readlines()
-        dist = str(lines).split('(')[0]
+        dist = str(lines).split("(")[0]
         cleanEntry(dist)
-        logging.info('Got dist info.')
+        logging.info("Got dist info.")
         return dist
 
+
 def getGPU():
-    """Get GPU information and return a list 
-    """
+    """Get GPU information and return a list"""
     cmd = "lspci -v -s $(lspci | grep ' VGA ' | cut -d ' ' -f 1)"
     standard_output = str(subprocess.getoutput(cmd))
     output = []
-    output.append(standard_output.split('\n\t'))
-    logging.info('Got GPU info.')
-        
+    output.append(standard_output.split("\n\t"))
+    logging.info("Got GPU info.")
+
+
 def getLinuxInfo():
-    """Aggregate Linux system info into a list and send to writeOutFile()
-    """
+    """Aggregate Linux system info into a list and send to writeOutFile()"""
     commands = [
-                (f'System: {platform.system()}'),
-                (getDist()),
-                (f'Architecture: {platform.architecture()[0]}'),
-                (f'Machine: {platform.machine()}'),
-                (f'Node: {platform.node()}'),
-                (f'CPU: {getCPU()}'),
-                (f'GPU: {getGPU()}'),
-                (getMem()[0]), # Used  memory
-                (getMem()[1])  # Available memory
-                ]
+        (f"System: {platform.system()}"),
+        (getDist()),
+        (f"Architecture: {platform.architecture()[0]}"),
+        (f"Machine: {platform.machine()}"),
+        (f"Node: {platform.node()}"),
+        (f"CPU: {getCPU()}"),
+        (f"GPU: {getGPU()}"),
+        (getMem()[0]),  # Used  memory
+        (getMem()[1]),  # Available memory
+    ]
     info_dump = []
-    logging.info('Running commands...')
+    logging.info("Running commands...")
     for command in commands:
-        info_dump.append(str(command).split('\r'))
+        info_dump.append(str(command).split("\r"))
     for entry in info_dump:
-        logging.debug('Sanitizing entry...')
+        logging.debug("Sanitizing entry...")
         entry = cleanEntry(entry)
         writeOutfile(entry)
+
 
 def getWinInfo():
-    """Get Windows system information and write it to a file
-    """
-    raw_info = subprocess.check_output(['systeminfo']).decode('utf-8').split('\n')
+    """Get Windows system information and write it to a file"""
+    raw_info = subprocess.check_output(["systeminfo"]).decode("utf-8").split("\n")
     info_dump = []
     for item in raw_info:
-        info_dump.append(str(item.split('\r')[:-1]))
+        info_dump.append(str(item.split("\r")[:-1]))
     for entry in info_dump:
         entry = cleanEntry(entry)
         writeOutfile(entry)
-    
+
+
 def cleanEntry(entry):
-    """Sanitize each line of the output before writing
-    """
-    cleaned_entry = (str(entry).strip('[\'\"]')).strip() # Remove leading and trailing whitespaces and Python-related [] and '
-    cleaned_entry = " ".join(cleaned_entry.split()) # Remove consecutive whitespace characters (to make output smaller onscreen)
-    logging.info('Sanitizing entry...')
-    return cleaned_entry # Returns one individual entry, *NOT* the entire outfile; there might be a better way to do this
+    """Sanitize each line of the output before writing"""
+    cleaned_entry = (
+        str(entry).strip("['\"]")
+    ).strip()  # Remove leading and trailing whitespaces and Python-related [] and '
+    cleaned_entry = " ".join(
+        cleaned_entry.split()
+    )  # Remove consecutive whitespace characters (to make output smaller onscreen)
+    logging.info("Sanitizing entry...")
+    return cleaned_entry  # Returns one individual entry, *NOT* the entire outfile; there might be a better way to do this
+
 
 def writeOutfile(line):
-    """Write the system information output file to WINFO.
-    """
+    """Write the system information output file to SYS_INFO.txt."""
     # TODO: Send this outfile to the root dir
     # TODO: Or, maybe make it write to a tmp file here then format it later?
-    logging.debug('Writing outfile...')
-    with open(SYS_INFO, 'a') as output:
+    logging.debug("Writing outfile...")
+    with open(SYS_INFO, "a") as output:
         output.write(line)
-        output.write('\r')
-    logging.debug('Done.')
+        output.write("\r")
+    logging.debug("Done.")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     isPlatformSupported()
